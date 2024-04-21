9b07cd29d88db45987c6e1a0f2c9c200a1ec8913
sharma5974 <sharma@appi-fy.ai>
2024-04-21 16:17:58 +0530

    commit all junk files

--- appify.dbroutine/DBStoredProc.cs
+++ appify.dbroutine/DBStoredProc.cs
@@ -97,10 +97,6 @@ namespace appify.dbroutine
         public const string LISTALLPRODUCT = "[Operation].[usp_ProductMasterListAll]";
         public const string UPDATEPRODUCTIMAGEPRICE = "[Operation].[usp_ProductMasterUpdatePriceImage]";
 
-         
-        public const string LISTNEWPRODUCTS = "[Operation].[usp_ProductMasterIsNewStatus]";
-        public const string UPDATENEWPRODUCTS = "[Operation].[usp_ProductMasterUpdateIsNew]";
-
 
         /// <summary>
         /// [Operation].[ProductImage]
