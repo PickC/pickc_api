0c81905427dd2e8941a775d900a644ce2edfec23
Gurjeet-Appi-Fy-AI <166098618+Gurjeet-Appi-Fy-AI@users.noreply.github.com>
2024-10-22 22:56:50 +0530

    Update IOrderBusiness.cs

--- appify.Business.Contract/IOrderBusiness.cs
+++ appify.Business.Contract/IOrderBusiness.cs
@@ -39,7 +39,6 @@ namespace appify.Business.Contract
         public Int64 UpdateOrderTrackingStatus(OrderTrackingUpdate item);
         public Int64 UpdateDelhiveryOrderTrackingStatus(OrderTrackingUpdateDelhivery item);
         public bool OrderPaymentSave(OrderPayment item);
-
         public List<DailyOrderSummary> GetDailyOrderSummary();
     }
 }
