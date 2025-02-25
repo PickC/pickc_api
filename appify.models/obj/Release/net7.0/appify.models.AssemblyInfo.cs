0eed8ddf81ea9c1e5ac2b7037bba8d07b0073da2
Gurjeet Singh <gurjeet@appi-fy.ai>
2025-02-25 19:51:08 +0530

    Updated Code with few updation on Order/getitemnew API

--- appify.models/obj/Release/net7.0/appify.models.AssemblyInfo.cs
+++ appify.models/obj/Release/net7.0/appify.models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appify.models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49fde0704edf4623fe20d5d5f0c29990af12838e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15b232aaea99e0349d668e9a0602e4807745f93b")]
 [assembly: System.Reflection.AssemblyProductAttribute("appify.models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appify.models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
