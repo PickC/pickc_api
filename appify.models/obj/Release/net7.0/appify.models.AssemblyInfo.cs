743bcd417786ce1ef1a6a668d699865586d9cede
Gurjeet Singh <gurjeet@appi-fy.ai>
2025-03-22 17:26:22 +0530

    updated code

--- appify.models/obj/Release/net7.0/appify.models.AssemblyInfo.cs
+++ appify.models/obj/Release/net7.0/appify.models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appify.models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef2064ee08433bd8b95f52a01a2e0005791efd1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8032c73f07e2c7294d26c5118403e08e31af19f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("appify.models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appify.models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
