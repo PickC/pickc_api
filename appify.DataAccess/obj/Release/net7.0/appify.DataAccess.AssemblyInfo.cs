99c25d435878d61acb4434c141de53f241789b98
ragsarma <ragsarma@gmail.com>
2025-07-29 19:03:00 +0530

    recommit

--- appify.DataAccess/obj/Release/net7.0/appify.DataAccess.AssemblyInfo.cs
+++ appify.DataAccess/obj/Release/net7.0/appify.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appify.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24e3a516c607489419c43e39032d081e4efe389")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0903a7d12012ab44ef8d7f09d613f84a703b8aa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("appify.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appify.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
